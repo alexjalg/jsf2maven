6ee3e558d72e9fbdfdfd9588996512469d58fb6c
Alejandro <alaura@3psi.com.pe>
2016-02-11 00:25:07 -0500

    Practimos como renderizar datos en una tabla

--- src/main/java/com/alexjalg/beans/PersonaBean.java
+++ src/main/java/com/alexjalg/beans/PersonaBean.java
@@ -1,15 +1,42 @@
 package com.alexjalg.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+
+import clases.Persona;
 
 @ManagedBean
 @RequestScoped
 public class PersonaBean {
-
+	
 	private String nombre;
 	private String mensaje;
+	private Persona persona = new Persona();
+	private static List<Persona> lstpersona = new ArrayList<Persona>();
 	
+	public void agregarPersona(){
+		PersonaBean.lstpersona.add(this.persona);
+	}
+	
+	public void eliminarPersona(Persona persona){
+		PersonaBean.lstpersona.remove(persona);
+	}
+	
+	public Persona getPersona() {
+		return persona;
+	}
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+	public List<Persona> getLstpersona() {
+		return lstpersona;
+	}
+	public void setLstpersona(List<Persona> lstpersona) {
+		PersonaBean.lstpersona = lstpersona;
+	}
 	public String getNombre() {
 		return nombre;
 	}
